7b8bce3f4cc1deb6d5d4ea2da03e092617b495d8
WhoSoup <who.soup@gmail.com>
2020-06-01 16:29:06 +0200

    loop and config rewrite

--- monitor.go
+++ monitor.go
@@ -3,31 +3,43 @@ package monitor
 import (
 	"context"
 	"sync"
+	"time"
 
 	"github.com/AdamSLevy/jsonrpc2"
 )
 
+var Interval time.Duration = time.Second
+
 type Monitor struct {
-	conf   Config
+	url    string
 	client *jsonrpc2.Client
 
 	height int64
 	minute int64
 
+	listenerMtx sync.Mutex
+	listeners   []chan Event
+
 	close  chan interface{}
 	closer sync.Once
-
-	cancel context.CancelFunc
 }
 
-func NewMonitor(conf *Config) (*Monitor, error) {
+type Event struct {
+	Err          error
+	DBHeight     int64
+	LeaderHeight int64
+	Minute       int64
+}
+
+func NewMonitor(url string) (*Monitor, error) {
 	m := new(Monitor)
-	m.conf = *conf // can't be changed from outside
+	m.url = url
+
 	m.client = new(jsonrpc2.Client)
 
 	ctx, cancel := context.WithCancel(context.Background())
-	m.cancel = cancel
-	response, err := m.Poll(ctx)
+	defer cancel()
+	response, err := m.CurrentMinute(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -37,28 +49,88 @@ func NewMonitor(conf *Config) (*Monitor, error) {
 
 	m.close = make(chan interface{})
 
-	go m.run()
+	go m.run(response)
 	return m, nil
 }
 
-func (m *Monitor) run() {
-	for {
+func (m *Monitor) NewListener() <-chan Event {
+	m.listenerMtx.Lock()
+	defer m.listenerMtx.Unlock()
+	l := make(chan Event, 20)
+	m.listeners = append(m.listeners, l)
+	return l
+}
+
+func (m *Monitor) run(resp *MinuteResponse) {
+	for resp != nil {
+		// wait for next minute
+		defMinute := time.Duration(resp.DBlockSeconds) * time.Second / 10
+		minuteStart := time.Unix(resp.MinuteStartTime, 0)
+		serverTime := time.Unix(resp.Time, 0)
+
+		wait := serverTime.Add(defMinute).Sub(minuteStart)
 		select {
 		case <-m.close:
 			return
-		default:
+		case <-time.After(wait):
 		}
 
-		ctx, cancel := context.WithCancel(context.Background())
-		m.cancel = cancel
-		m.Poll(ctx)
-
+		if resp = m.pollLoop(); resp != nil {
+			m.notify(resp)
+		}
 	}
 }
 
-func (m *Monitor) Poll(ctx context.Context) (*MinuteResponse, error) {
+func (m *Monitor) send(event Event) {
+	m.listenerMtx.Lock()
+	defer m.listenerMtx.Lock()
+	for _, l := range m.listeners {
+		select {
+		case l <- event:
+		default: // nobody is reading
+		}
+	}
+}
+func (m *Monitor) notify(resp *MinuteResponse) {
+	var e Event
+	e.DBHeight = resp.DBHeight
+	e.Minute = resp.Minute
+	m.send(e)
+}
+
+func (m *Monitor) notifyErr(err error) {
+
+}
+
+func (m *Monitor) pollLoop() *MinuteResponse {
+	for {
+		resp := m.poll()
+		if resp != nil {
+			return resp
+		}
+
+		select {
+		case <-m.close:
+			return nil
+		case <-time.After(Interval):
+		}
+	}
+}
+
+func (m *Monitor) poll() *MinuteResponse {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	res, err := m.CurrentMinute(ctx)
+	if err != nil {
+		m.notifyErr(err)
+		return nil
+	}
+	return res
+}
+
+func (m *Monitor) CurrentMinute(ctx context.Context) (*MinuteResponse, error) {
 	res := new(MinuteResponse)
-	if err := m.client.Request(ctx, m.conf.FactomdURL, "current-minute", nil, res); err != nil {
+	if err := m.client.Request(ctx, m.url, "current-minute", nil, res); err != nil {
 		return nil, err
 	}
 	return res, nil
@@ -67,6 +139,5 @@ func (m *Monitor) Poll(ctx context.Context) (*MinuteResponse, error) {
 func (m *Monitor) Stop() {
 	m.closer.Do(func() {
 		close(m.close)
-		m.cancel()
 	})
 }
